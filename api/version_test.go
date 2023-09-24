c775797cb0ea2f6127701dd873e3e8d26f84e664
Christoph Vilsmeier <vilsmeier@gmx.de>
2023-09-24 20:17:52 +0200

    dist script

--- api/version_test.go
+++ api/version_test.go
@@ -15,4 +15,12 @@ func TestVersion(t *testing.T) {
 	rest, _, found = strings.Cut(rest, " ")
 	assertEq(t, true, found)
 	assertEq(t, Version, "v"+rest)
+	// parse scripts/dist.sh
+	data, err = os.ReadFile("../scripts/dist.sh")
+	assertNil(t, err)
+	_, rest, found = strings.Cut(string(data), "VERSION=\"")
+	assertEq(t, true, found)
+	rest, _, found = strings.Cut(rest, "\"")
+	assertEq(t, true, found)
+	assertEq(t, Version, rest)
 }
